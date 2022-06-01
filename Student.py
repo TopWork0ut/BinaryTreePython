bfa9e6f1391029f8825c74ec8063d220870c5bf1
TopWork0ut <olegiksen@gmail.com>
2022-06-01 21:07:43 +0300

    deleted unnesessary getters and setters

--- Student.py
+++ Student.py
@@ -1,25 +1,14 @@
 class Student:
     def __init__(self, name: str, surname: str, group: int, year_of_birth: int, rating: int):
-        self._name = name
-        self._surname = surname
-        self._group = group
-        self._year_of_birth = year_of_birth
-        self._rating = rating
+        self.name = name
+        self.surname = surname
+        self.group = group
+        self.year_of_birth = year_of_birth
+        self.rating = rating
 
     def __str__(self) -> str:
-        return f"Name: {self._name}, rating: {self._rating}, group:{self._group}"
+        return f"Name: {self.name}, rating: {self.rating}, group:{self.group}"
 
-    @property
-    def rating(self):
-        return self._rating
-
-    @rating.setter
-    def rating(self, rating):
-        self._rating = rating
-    #
-    @property
-    def group(self):
-        return self._group
 
 
 
